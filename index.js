aa7965c6f70def1eba43eb48e849f0464a0c86fa
sylvia <mogotusylvia@gmail.com>
2023-10-26 11:54:26 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-document.getElementById("btn").addEventListener("click", function () {
-    window.location.href = "success.html";
+"use strict";
+const fom = document.querySelector('form');
+console.log("silvia");
+fom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let input = document.querySelector('input');
+    let my_value = input.value;
+    const error_message = document.querySelector('.error_message');
+    const input_ele = document.querySelector('#input');
+    my_value = my_value.toString();
+    if ((my_value.includes("@")) && (my_value.includes(".")) && ((my_value.trim()).length != 0)) {
+        // The email is valid
+        error_message.innerHTML = "";
+        error_message.style.color = "";
+        input_ele.style.border = "";
+        input_ele.style.background = "";
+        window.location.href = "success.html";
+    }
+    else {
+        // The email is invalid
+        error_message.innerHTML = "Invalid email ";
+        error_message.style.color = "red";
+        input_ele.style.border = "solid red";
+        input_ele.style.background = "red";
+    }
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,32 @@
+const fom = document.querySelector('form') as HTMLFormElement;
+
+console.log("silvia");
 
 
-document.getElementById("btn").addEventListener("click", function() {
-    window.location.href = "success.html";
-  });
+
+fom.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let input = document.querySelector('input') as HTMLInputElement;
+    let my_value: string = input.value;
+    const error_message = document.querySelector('.error_message') as HTMLParagraphElement;
+    const input_ele = document.querySelector('#input') as HTMLInputElement;
+
+    my_value  = my_value.toString()
+
+    if ((my_value.includes("@")) && (my_value.includes(".")) && ((my_value.trim()).length != 0)) {
+      // The email is valid
+      error_message.innerHTML = "";
+      error_message.style.color = "";
+      input_ele.style.border = "";
+      input_ele.style.background = "";
+      window.location.href = "success.html";
+    }
+    else {
+      // The email is invalid
+      error_message.innerHTML = "Invalid email ";
+      error_message.style.color = "red";
+      input_ele.style.border = "solid red";
+      input_ele.style.background = "red";
+    }
   
+});
